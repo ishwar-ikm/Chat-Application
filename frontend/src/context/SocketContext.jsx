0d6c70819e81934d29756b64ce33724e45234d11
Ishwar Mishra <ikmishra2002@gmail.com>
2024-06-21 22:22:48 +0530

    UPDATE socket-url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://127.0.0.1:5000", {
+            const socket = io("https://chat-application-klfk.onrender.com/", {
                 query: {
                     userId: authUser._id,
                 },
