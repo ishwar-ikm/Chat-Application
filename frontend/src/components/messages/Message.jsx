081e21190f6657f37b731ec5d49c5482fe6d8641
Ishwar Mishra <ikmishra2002@gmail.com>
2024-06-21 21:38:21 +0530

    fix message alignment bug

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -12,8 +12,12 @@ const extractTime = (dateString) => {
 const Message = ({message}) => {
     const {authUser} = useAuthContext();
     const {selectedConversation} = useConversation();
+    const fromMe = message.senderId === authUser._id;
+    const chatClassName = fromMe ? "chat-end" : "chat-start";
+
+
     return (
-        <div className={`chat chat-${message.senderId == authUser._id ? "end" : "start"}`}>
+        <div className={`chat ${chatClassName}`}>
             <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
                     <img alt="Tailwind CSS chat bubble component" src={message.senderId == authUser._id ? `${authUser.profilePic}` : `${selectedConversation.profilePic}`} />
