4bc23cf8c5b5d1296071c14c1d4bd640cd17673d
Ishwar Mishra <ikmishra2002@gmail.com>
2024-06-21 22:30:26 +0530

    fix time not visible on last message

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -23,7 +23,7 @@ const Message = ({message}) => {
                     <img alt="Tailwind CSS chat bubble component" src={message.senderId == authUser._id ? `${authUser.profilePic}` : `${selectedConversation.profilePic}`} />
                 </div>
             </div>
-            <div className={`${message.shouldShake ? "shake" : ""} chat-bubble chat-bubble-${message.senderId == authUser._id ? "primary" : ""}`}>{message.message}</div>
+            <div className={`${message.shouldShake ? "shake" : ""} pb-2 chat-bubble chat-bubble-${message.senderId == authUser._id ? "primary" : ""}`}>{message.message}</div>
             <div className="chat-footer opacity-60">
                 {extractTime(message.createdAt)}
             </div>
